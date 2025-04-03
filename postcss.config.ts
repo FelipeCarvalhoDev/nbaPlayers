7fc9843420789553d29328ba2977943dcb24c9c6
Felipe Carvalho <felipelima.web@gmail.com>
2025-04-03 18:21:52 -0300

    Estrutura Inicial Corrigido

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,6 +1,8 @@
 export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {
+      tailwindConfig: './tailwind.config.ts'
+    },
+    'autoprefixer': {}
   }
 }
